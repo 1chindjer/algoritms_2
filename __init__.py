0768e59ba827912e25f55f782e5279fc54a5e859
1chindjer <chindjer@gmail.com>
2023-08-06 19:23:52 +0300

    разворачиваем список

---
+++ __init__.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+# печатаем список
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data, end=" -> ")
+            cur_node = cur_node.next
+        print("None")
+# разворачиваем список
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+#  создаем список из элементов циклом
+llist = LinkedList()
+for i in range(10):
+    llist.append(i/2)
+
+
+print("Оригинальный список:")
+llist.print_list()
+
+llist.reverse()
+
+print("\nРазвернутый список:")
+llist.print_list()
